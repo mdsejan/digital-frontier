ad175a552e22848effea9979ca270efb019b3e27
Sejan~ <md4sejan@gmail.com>
2024-03-29 20:09:38 +0600

    routes created and make menu component

---
+++ app/work/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <h2 className="text-6xl font-bold">Work page</h2>
+    </div>
+  );
+};
+
+export default page;
